5e97a8e6edf056bf4efafee00b41a4bd76c5e43c
Timothy Nott <timothynott@ffrf.org>
2017-04-15 12:58:55 -0500

    second commit of ionic stuff

---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+import { FlightPage } from '../pages/flight/flight';
+
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage,
+    FlightPage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp),
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage,
+    FlightPage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
---
+++ src/pages/flight/flight.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+
+@Component({
+    selector: "page-flight",
+    templateUrl: "flight.html"
+})
+export class FlightPage{
+    constructor(public navCtrl: NavController) {
+
+    }
+}

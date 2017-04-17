97afaa763b288864c267b82e16f9e84a657fbf29
Timothy Nott <timothynott@ffrf.org>
2017-04-16 20:46:00 -0500

    added flights by API

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,17 +1,24 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
 import {FlightPage} from '../flight/flight';
+import {FlightsService} from '../../providers/flights-service';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-  flights: Array<{id: number, league: number, name: string, year: number, ntrp: string, start_date: string}>;
+  flights: any; //Array<{id: number, league: number, name: string, year: number, ntrp: string, start_date: string}>;
 
-  constructor(public navCtrl: NavController) {
-    this.flights = [{"id":1,"league":1,"name":"3.0 Women","year":2017,"ntrp":"3.0","start_date":"2017-04-17"},{"id":2,"league":1,"name":"3.5/4.0 Women","year":2017,"ntrp":"3.5, 4.0","start_date":"2017-04-17"},{"id":3,"league":1,"name":"3.0 Men","year":2017,"ntrp":"3.0","start_date":"2017-04-17"},{"id":4,"league":1,"name":"3.5 Men Flight A","year":2017,"ntrp":"3.5","start_date":"2017-04-17"},{"id":5,"league":1,"name":"3.5 Men Flight B","year":2017,"ntrp":"3.5","start_date":"2017-04-17"},{"id":6,"league":1,"name":"3.5 Men Flight C","year":2017,"ntrp":"3.5","start_date":"2017-04-17"},{"id":7,"league":1,"name":"4.0 Men Flight A","year":2017,"ntrp":"4.0","start_date":"2017-04-17"},{"id":8,"league":1,"name":"4.0 Men Flight B","year":2017,"ntrp":"4.0","start_date":"2017-04-17"}];
+  constructor(public navCtrl: NavController, private Flights: FlightsService) {
+    this.flights = [];
+  }
+
+  public ionViewWillEnter() {
+    this.Flights.loadFlights()
+      .then(data => {
+        this.flights = data;
+      });
   }
 
   flightTapped(event, flight) {
--- src/providers/flights-service.ts
+++ src/providers/flights-service.ts
@@ -11,8 +11,84 @@ import 'rxjs/add/operator/map';
 @Injectable()
 export class FlightsService {
 
-  constructor(public http: Http) {
-    console.log('Hello FlightsService Provider');
+  flights: any;
+  flightMatches: any;
+  flightPlayers: any;
+
+  constructor(private http: Http) {
+    //console.log('Hello FlightsService Provider');
+    this.flights = [];
+    this.flightMatches = [];
+    this.flightPlayers = [];
+  }
+
+  loadFlights() {
+    if (this.flights.length) {
+      return Promise.resolve(this.flights);
+    }
+
+    return new Promise(resolve => {
+      this.http.get('/api/flights')
+        .map(res => res.json())
+        .subscribe(data => {
+          this.flights = data;
+          resolve(this.flights);
+        });
+    });
+  }
+
+  loadFlightMatches(id: number) {
+    let flightMatchTest = this.checkFlightMatches(id);
+    if(!flightMatchTest) {
+      return new Promise(resolve => {
+        this.http.get('/api/flights/' + id + '/matches')
+          .map(res => res.json())
+          .subscribe(data => {
+            this.flightMatches.push({flightId: id, matches: data});
+            resolve(data);
+          });
+      });
+    }
+    else {
+      return Promise.resolve(flightMatchTest);
+    }
+  }
+
+  checkFlightMatches(id: number) {
+    // look through the gathered flight matches to see if one matches
+    this.flightMatches.forEach(element => {
+      if(element.flightId === id) {
+        return element.matches;
+      }
+    });
+    return false;
+  }
+
+  loadFlightPlayers(id: number) {
+    let flightPlayersTest = this.checkFlightPlayers(id);
+    if(!flightPlayersTest) {
+      return new Promise(resolve => {
+        this.http.get('/api/flights/' + id + '/players')
+          .map(res => res.json())
+          .subscribe(data => {
+            this.flightPlayers.push({flightId: id, players: data});
+            resolve(data);
+          });
+      });
+    }
+    else {
+      return Promise.resolve(flightPlayersTest);
+    }
+  }
+
+  checkFlightPlayers(id: number) {
+    // look through the gathered flight players to see if one matches
+    this.flightPlayers.forEach(element => {
+      if(element.flightId === id) {
+        return element.players;
+      }
+    });
+    return false;
   }
 
 }
